fab383d78d2f998bc2ae878a383629ce04225659
Alon Shprung <alon@cherrypick-consulting.com>
2021-09-29 14:40:54 +0300

    fire analytics event - ios

--- ios/Spotim.swift
+++ ios/Spotim.swift
@@ -17,31 +17,40 @@ import SpotImCore
     @objc func viewHeightDidChange(to newValue: CGFloat)
 }
 
+public protocol SPAnalyticsEventDelegate: AnyObject {
+    func trackEvent(type: SPEventType, event: SPEventInfo)
+}
+
 @objc public enum SpotImUserInterfaceStyle: Int {
     case light, dark
 }
 
 @objc(SpotImBridge)
-public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.SpotImLayoutDelegate {
-    
+public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.SpotImLayoutDelegate, SpotImCore.SPAnalyticsEventDelegate {
+
     var spotImCoordinator: SpotImSDKFlowCoordinator!
-    
+
     @objc public func startLoginFlow() {
         NotificationCenter.default.post(name: Notification.Name("StartLoginFlow"), object: nil)
     }
-    
+
     @objc public func viewHeightDidChange(to newValue: CGFloat) {
         NotificationCenter.default.post(name: Notification.Name("ViewHeightDidChange"), object: String(describing: newValue))
     }
     
+    public func trackEvent(type: SPEventType, event: SPEventInfo) {
+        NotificationCenter.default.post(name: Notification.Name("TrackAnalyticsEvent"), object: getAnalyticsEventAsDictionary(event: event))
+    }
+
     @objc public func initialize(_ spotId: String) {
         SpotIm.initialize(spotId: spotId)
+        SpotIm.setAnalyticsEventDelegate(delegate: self)
     }
-    
+
     @objc public func setBackgroundColor(_ red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
         SpotIm.darkModeBackgroundColor = UIColor(red: red, green: green, blue: blue, alpha: alpha);
     }
-    
+
     @objc public func overrideUserInterfaceStyle(style: SpotImUserInterfaceStyle) {
         switch style {
         case .light:
@@ -50,7 +59,7 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
             SpotIm.overrideUserInterfaceStyle = .dark
         }
     }
-    
+
     @objc public func getConversationCounters(_ conversationIds: Array<String>,
                                                 completion: @escaping (String) -> Void,
                                                 onError: @escaping (Error) -> Void) {
@@ -65,11 +74,11 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
             }
         }
     }
-    
+
     @objc public func createSpotImFlowCoordinator(_ loginDelegate: Any,
                                                     completion: @escaping () -> Void,
                                                     onError: @escaping (Error) -> Void) {
-        
+
         SpotIm.createSpotImFlowCoordinator(loginDelegate: self) { result in
             switch result {
                 case .success(let coordinator):
@@ -83,7 +92,7 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
             }
         }
     }
-    
+
     @objc public func getPreConversationController(_ nc: UINavigationController,
                                                    postId: String,
                                                    url: String,
@@ -92,15 +101,15 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
                                                    thumbnailUrl: String,
                                                    completion: @escaping (UIViewController) -> Void,
                                                    onError: @escaping (Error) -> Void) {
-        
+
         let articleMetadata: SpotImArticleMetadata = SpotImArticleMetadata.init(url: url, title: title, subtitle: subtitle, thumbnailUrl: thumbnailUrl)
-        
+
         self.spotImCoordinator?.preConversationController(withPostId: postId, articleMetadata: articleMetadata, navigationController: nc, completion: { preConversationVC in
              completion(preConversationVC)
         })
-        
+
     }
-    
+
     @objc public func startSSO(_ completion: @escaping ([String:Any]) -> Void,
                                  onError: @escaping (Error) -> Void) {
         SpotIm.startSSO { response, error in
@@ -115,7 +124,7 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
             }
         }
     }
-    
+
     @objc public func completeSSO(_ with: String, completion: @escaping ([String:Any]) -> Void,
                                     onError: @escaping (Error) -> Void) {
         SpotIm.completeSSO(with: with) { success, error in
@@ -126,7 +135,7 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
             }
         }
     }
-    
+
     @objc public func sso(_ withJwtSecret: String, completion: @escaping ([String:Any]) -> Void,
                             onError: @escaping (Error) -> Void) {
         SpotIm.sso(withJwtSecret: withJwtSecret) { response, error in
@@ -141,7 +150,7 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
             }
         }
     }
-    
+
     @objc public func getUserLoginStatus(_ completion: @escaping ([String:Any]) -> Void,
                                            onError: @escaping (Error) -> Void) {
         SpotIm.getUserLoginStatus(completion: { result in
@@ -155,7 +164,7 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
             }
         })
     }
-    
+
     @objc public func logout(_ completion: @escaping ([String:Any]) -> Void,
                                onError: @escaping (Error) -> Void) {
         SpotIm.logout(completion: { result in
@@ -170,6 +179,22 @@ public class SpotImBridge: NSObject, SpotImCore.SpotImLoginDelegate, SpotImCore.
         })
     }
     
+    private func getAnalyticsEventAsDictionary(event: SPEventInfo)-> [String: Any] {
+        let eventAsDic = dictionary(encodable: event)
+        if var eventAsDic = eventAsDic {
+            // change key "event_type" to "type"
+            eventAsDic["type"] = eventAsDic["event_type"]
+            eventAsDic.removeValue(forKey: "event_type")
+            // change key "engine_status_type" to "engine_status"
+            eventAsDic["engine_status"] = eventAsDic["engine_status_type"]
+            eventAsDic.removeValue(forKey: "engine_status_type")
+            return eventAsDic
+        } else {
+            return [:]
+        }
+        
+    }
+
     private func dictionary<T>(encodable: T) -> [String: Any]? where T : Encodable {
         let encoder = JSONEncoder()
         encoder.keyEncodingStrategy = .convertToSnakeCase
